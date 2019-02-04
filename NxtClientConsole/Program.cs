999cad68daccad6702ea77a79dd49b210e286af0
Craig Woollett <craigwoollett@outlook.com>
2019-02-04 17:25:52 +1100

    Add project files.

---
+++ NxtClientConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using MyNamespace;
+using System;
+
+namespace NxtClientConsole
+{
+    class Program
+    {
+        public static async void callMethod()
+        {
+            var client = new NXTClient();
+            var uuid = await client.GetTransactionIdAsync("");
+            Console.WriteLine("uuid");
+        }
+        static void Main(string[] args)
+        {
+
+
+            callMethod();
+            Console.ReadKey();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
